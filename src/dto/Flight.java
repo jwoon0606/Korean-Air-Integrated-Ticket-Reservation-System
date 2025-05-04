83383ba4e14ae2e6103e454b11470ddf9eb555fe
jwoon0606 <jwoon0606@gmail.com>
2025-05-04 19:20:36 +0900

    Create Flight Class

---
+++ src/dto/Flight.java
@@ -0,0 +1,57 @@
+package dto;
+
+public class Flight {
+    private String flightNumber;
+    private String departure;
+    private String destination;
+    private String departureTime;
+    private String arrivalTime;
+
+    public Flight(String flightNumber, String departure, String destination, String departureTime, String arrivalTime) {
+        this.flightNumber = flightNumber;
+        this.departure = departure;
+        this.destination = destination;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+}
